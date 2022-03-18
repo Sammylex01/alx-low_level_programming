5711d9f4f2ec0e0e4b4704f2e68bd39d0979dc0d
Samdroid <samueloyekoya12@gmail.com>
2022-03-18 03:20:03 +0100

    Update 2-print_alphabet_x10.c
    Fix

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,8 +5,23 @@
  *
  * Return: Always 0.
  */
-int main(void)
+void print_alphabet_x10(void)
 {
-    print_alphabet_x10();
-    return (0);
+int alphabet;
+int count;
+
+
+count = 0;
+while (count < 10)
+{
+for (alphabet = 'a' ; alphabet <= 'z'; alphabet++)
+{
+_putchar(alphabet);
+}
+
+count++;
+_putchar('\n');
+}
+
+
 }
