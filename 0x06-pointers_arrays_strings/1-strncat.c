767b7c514bb12ab68113e68acfc19369c98fd7f2
Samdroid <samueloyekoya12@gmail.com>
2022-03-25 02:03:00 +0100

    Update 1-strncat.c
    Fix

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,26 +1,24 @@
-#include "holberton.h"
-
+#include "main.h"
 /**
- * _strncat - concatenates two strings
- * @dest: destination char
- * @src: source char
- * @n: number of bytes from src
- * Return: new string
- */
+ * *_strncat - concatenates n bytes of two strings
+ * @dest: pointer destination
+ * @src: pointer source
+ * @n: number of bytes
+ * Return: void
+*/
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int i;
-	int len = 0;
 
-	while (dest[len] != '\0')
-	{
-		len++;
-	}
+int dest_len, i;
+for (dest_len = 0; dest[dest_len] != '\0'; dest_len++)
+;
 
-	for (i = 0 ; i < n && src[i] != '\0' ; i++)
-		dest[len + i] = src[i];
-	dest[len + i] = '\0';
+for (i = 0; i < n && src[i] != '\0'; i++)
+	dest[dest_len + i] = src[i];
 
-	return (dest);
+
+dest[dest_len + i] = '\0';
+
+return (dest);
 }
